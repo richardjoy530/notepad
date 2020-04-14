8ab40505656dbfaf8794677d1e47decd92ba6032
Richard Joy <richardjoy530@gmail.com>
2020-04-14 10:59:51 +0530

    Home page under construction

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,154 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyTabbedHome());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyTabbedHome extends StatefulWidget {
+  @override
+  _MyTabbedHomeState createState() => _MyTabbedHomeState();
+}
+
+class _MyTabbedHomeState extends State<MyTabbedHome>
+    with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[
+    Tab(
+      //text: 'Categories',
+      icon: Icon(
+        Icons.category,
+        color: Colors.pinkAccent[400],
+      ),
+    ),
+    Tab(
+      //text: 'Notes',
+      icon: Icon(
+        Icons.note,
+        color: Colors.pinkAccent[400],
+      ),
+    ),
+    Tab(
+      //text: 'Starred',
+      icon: Icon(
+        Icons.star_half,
+        color: Colors.pinkAccent[400],
+      ),
+    ),
+  ];
+
+  TabController _tabController;
+  void _handleTabSelection() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: myTabs.length);
+    _tabController.addListener(_handleTabSelection);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      theme: ThemeData(fontFamily: 'SpaceMono'),
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[900],
+          bottom: TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.pinkAccent[400],
+            tabs: <Widget>[
+              Tab(
+                //text: 'Categories',
+                icon: Icon(
+                  Icons.category,
+                  color: _tabController.index == 0
+                      ? Colors.pinkAccent[400]
+                      : Colors.grey[500],
+                ),
+              ),
+              Tab(
+                //text: 'Notes',
+                icon: Icon(
+                  Icons.note,
+                  color: _tabController.index == 1
+                      ? Colors.pinkAccent[400]
+                      : Colors.grey[500],
+                ),
+              ),
+              Tab(
+                //text: 'Starred',
+                icon: Icon(
+                  Icons.star_half,
+                  color: _tabController.index == 2
+                      ? Colors.pinkAccent[400]
+                      : Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+          leading: Icon(
+            Icons.menu,
+            size: 30,
+            color: Colors.grey[500],
+          ),
+          title: Text(
+            'Categories',
+            style: TextStyle(
+              fontFamily: 'SpaceMono',
+              color: Colors.white,
+              fontSize: 20,
+              letterSpacing: 6,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.cloud_queue,
+                size: 30,
+                color: Colors.grey[500],
+              ),
+            )
+          ],
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: <Widget>[
+            Tab(
+              icon: Icon(
+                Icons.category,
+                color: Colors.pinkAccent[400],
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.home,
+                color: Colors.pinkAccent[400],
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.star,
+                color: Colors.pinkAccent[400],
+              ),
             ),
           ],
         ),
+        floatingActionButton: new FloatingActionButton(
+          backgroundColor: Colors.pinkAccent[400],
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
